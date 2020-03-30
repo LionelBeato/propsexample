8a8cb6a7ef27a000fd4863af60023d194417d0f4
Lionel Beato <leo@localhost.localdomain>
2020-03-30 14:57:38 -0400

    adds comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Fruit from './Fruit';
 function App() {
   return (
     <div>
+    {/* When we call our component, we can then define our props */}
+    {/* We can do this with as many instances of our Fruit component as we need to! */}
       <Fruit name="orange" color="orange" type="citrus" />
       <Fruit name="granny-smith" color="green" type="apple" />
     </div>
--- src/Fruit.js
+++ src/Fruit.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
-
+// note how we're passing 'props' as an argument into our component
+// this allows us to define component-level properties similar to HTML attributes 
 const Fruit = (props) => {
-
+    
     console.log("This component works!")
+
+    // props can be used anywhere in your code
     console.log(props.name, props.color, props.type)
 
     return (
         <div>
             <span>This is my fruit component</span>
+            {/* Here we are defining how we want to render our props */}
             <h2>{props.name}</h2>
+            {/* This color prop is being passed into our style property  */}
             <h2 style={{color:props.color}}>{props.color}</h2>
             <h2>{props.type}</h2>
         </div>
